fb4025b44c6d28edb0731df05dab8f8795ff161a
Jordan Kasper <jordan@jordankasper.com>
2018-10-14 19:38:40 -0400

    Added helper for the tag cloud and implemented tag cloud in sidebar partial

---
+++ helpers/tagcloud.js
@@ -0,0 +1,41 @@
+
+const debug = require('debug')('metalsmith-tagcloud');
+
+module.exports = function tagcloud(opts){
+  opts.key = opts.key || 'tags';
+
+  return function (files, metalsmith, done){
+    debug('tagcloud using metadata key: %s', opts.key);
+
+    const data = metalsmith.metadata();
+    data.tagcloud = [];
+    Object.keys(data[opts.key]).forEach(function(name) {
+      debug('Adding tag entry with data:', name, data[opts.key][name].urlSafe, data[opts.key][name].length);
+      data.tagcloud.push({
+        name,
+        slug: data[opts.key][name].urlSafe,
+        weight: data[opts.key][name].length,
+        weightName: getTagWeightName(data[opts.key][name].length)
+      })
+    });
+
+    done();
+  };
+
+  function getTagWeightName(count) {
+    let size = 'tiny';
+    count = count || 0;
+    if (count > 15) {
+        size = 'enormous';
+    } else if (count > 10) {
+        size = 'massive';
+    } else if (count > 7) {
+        size = 'large';
+    } else if (count > 4) {
+        size = 'medium';
+    } else if (count > 1) {
+        size = 'small';
+    }
+    return size;
+  }
+}
