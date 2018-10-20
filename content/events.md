7ffc296fe83dceb11b2059e765556edad85c64ca
Jordan Kasper <jordan@jordankasper.com>
2018-10-20 15:06:49 -0400

    Added events data, page, and sidebar content

---
+++ content/events.md
@@ -0,0 +1,4 @@
+---
+layout: events.hbs
+title: events
+---
---
+++ helpers/events.js
@@ -0,0 +1,50 @@
+
+const debug = require('debug')('metalsmith-events');
+const events = require('./events.json').events;
+
+module.exports = function tagcloud(opts){
+  opts.targetPast = opts.targetPast || 'pastEvents';
+  opts.targetFuture = opts.targetFuture || 'futureEvents';
+
+  return function (files, metalsmith, done){
+    debug('events module started with %s events', events.length);
+
+    const data = metalsmith.metadata();
+
+    data[opts.targetPast] = data[opts.targetPast] || [];
+    data[opts.targetFuture] = data[opts.targetFuture] || [];
+
+    var now = new Date();
+    now.setHours(0);
+
+    events.forEach(function(event) {
+      event.timestamp = (new Date(event.date)).getTime();
+    });
+    events
+      .filter(function(event) {
+        if (!event.timestamp) { debug('filtering out event due to bad timestamp (date=%s)', event.date); }
+        return event.timestamp;
+      })
+      .sort(function(a, b) {
+        a.timestamp = a.timestamp || (new Date(a.date)).getTime();
+        b.timestamp = b.timestamp || (new Date(b.date)).getTime();
+
+        return (a.timestamp - b.timestamp);
+      });
+
+    events.forEach(function(event) {
+      if (event.timestamp >= now.getTime()) {
+        data[opts.targetFuture].push(event);
+      } else {
+        data[opts.targetPast].push(event);
+      }
+    });
+
+    debug('Events processed...');
+    debug('  there are %s upcoming', data[opts.targetFuture].length);
+    debug('  there are %s past', data[opts.targetPast].length);
+
+    done();
+  };
+
+}
