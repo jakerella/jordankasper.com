2062e2e166ca19e6b11f6ed4ab2c9842b5718363
Jordan Kasper <jordan@jordankasper.com>
2018-10-28 18:21:59 -0400

    Fixed sorting of event data

--- helpers/events.js
+++ helpers/events.js
@@ -15,14 +15,14 @@ module.exports = function tagcloud(opts){
     data[opts.targetPast] = data[opts.targetPast] || [];
     data[opts.targetFuture] = data[opts.targetFuture] || [];
 
-    var now = new Date();
+    const now = new Date();
     now.setHours(0);
 
     events.forEach(function(event) {
       event.timestamp = (new Date(event.date)).getTime();
       event.monthDate = moment(event.timestamp).format('MMM Y');
     });
-    events
+    const sortedEvents = events
       .filter(function(event) {
         if (!event.timestamp) { debug('filtering out event due to bad timestamp (date=%s)', event.date); }
         return event.timestamp;
@@ -31,10 +31,10 @@ module.exports = function tagcloud(opts){
         a.timestamp = a.timestamp || (new Date(a.date)).getTime();
         b.timestamp = b.timestamp || (new Date(b.date)).getTime();
 
-        return (a.timestamp - b.timestamp);
+        return (b.timestamp - a.timestamp);
       });
 
-    events.forEach(function(event) {
+    sortedEvents.forEach(function(event) {
       if (event.timestamp >= now.getTime()) {
         data[opts.targetFuture].push(event);
       } else {
