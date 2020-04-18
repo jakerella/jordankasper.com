3cb9e8bf4aadc413ed32f6916da436b66a491255
Jordan Kasper <jordan@jordankasper.com>
2020-04-18 15:43:22 -0400

    Improved note taking formatter with list identification, continuation, and indentation

--- assets/static/notes/notes.js
+++ assets/static/notes/notes.js
@@ -1,29 +1,76 @@
-(function ($) {
+
+(() => {
     const bin = window.location.hash || 'misc';
-    $('.notes-title .bin').text(bin);
+    document.querySelector('.notes-title .bin').innerText = bin;
 
     const data = localStorage.getItem(bin);
 
-    const field = $('.edit-notes')
-        .val(data)
-        .keydown(function(e) {
-            if (e.keyCode === 9) {
-                e.preventDefault();
-            }
-        })
-        .keyup(function(e) {
-            if (e.keyCode === 9) {
-                const split = field[0].selectionStart;
-                field.val(
-                    field.val().substr(0, split) +
-                    '  ' +
-                    field.val().substr(split)
-                );
-                field[0].selectionStart = split + 2;
-                field[0].selectionEnd = split + 2;
-            }
-            localStorage.setItem(bin, field.val());
-        })
-        .focus();
+    const field = document.querySelector('.edit-notes');
 
-})(window.jQuery);
+    field.value = data;
+
+    field.addEventListener('keydown', (e) => {
+        // The tab would normally take us off of the textarea, we don't want that!
+        if (e.keyCode === 9) {
+            e.preventDefault();
+        }
+    });
+
+    field.addEventListener('keyup', (e) => {
+        // Insert 2-space tabs, including indenting list items
+        if (e.keyCode === 9) {
+            let insert = '  ';
+            let replace = false;
+            const current = getCurrentLine();
+            if (/^\s*(?:\*|\-)\s/.test(current.content)) {
+                insert += current.content;
+                replace = true;
+            }
+            insertElem(insert, replace);
+        }
+
+        // Handle automatic list continuation for * or -
+        if (e.keyCode === 13) {
+            const listElem = getListElem(1);
+            if (listElem) {
+                insertElem(listElem[1]);
+            }
+        }
+
+        localStorage.setItem(bin, field.value);
+    });
+
+    field.focus();
+
+    function insertElem(insert, replace=false) {
+        let start = field.selectionStart;
+        let end = field.selectionEnd;
+        if (replace) {
+            const current = getCurrentLine();
+            start = current.start;
+            end = current.end;
+        }
+        field.value = `${field.value.substr(0, start)}${insert}${field.value.substr(end)}`;
+        field.selectionStart = start + insert.length;
+        field.selectionEnd = start + insert.length;
+    }
+
+    function getListElem(linesBack=0) {
+        const split = field.selectionStart;
+        const lines = field.value.substr(0, split).split(/\n/);
+        const listElem = lines[lines.length-1-linesBack].match(/^(\s*(?:\*|\-)\s)[^\s\*\-]+/);
+        return listElem;
+    }
+
+    function getCurrentLine() {
+        const pos = field.selectionStart;
+        const pastLines = field.value.substr(0, pos).split(/\n/);
+        lineEnd = field.value.substr(pos).split(/\n/)[0];
+        return {
+            start: pos - pastLines[pastLines.length-1].length,
+            end: pos + lineEnd.length,
+            content: pastLines[pastLines.length-1] + lineEnd
+        }
+    }
+    window.getCurrentLine = getCurrentLine;
+})();
