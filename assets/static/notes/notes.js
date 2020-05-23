71c80a594a51332361ccdcec06f1cea291b6e465
Jordan Kasper <jordan@jordankasper.com>
2020-05-23 13:10:18 -0400

    Needed a way to get back deleted info, so added versioning

--- assets/static/notes/notes.js
+++ assets/static/notes/notes.js
@@ -1,10 +1,14 @@
 
 (() => {
+    const VERSION_DELAY =  (1000 * 60 * 3);
+    const MAX_VERSIONS = 15;
+    const VERSION_SUFFIX = '__versions';
     let currentBin = window.location.hash.substr(1) || 'misc';
     const field = document.querySelector('.edit-notes');
 
     setupEditor();
-    addBinToggle();
+    setupBinVersioning();
+    setupTopMenus();
     field.focus();
 
 
@@ -52,9 +56,57 @@
         });
     }
 
-    function addBinToggle() {
-        const list = document.querySelector('.notes-list');
-        list.innerHTML += buildBinList();
+    function getVersions() {
+        let versions = localStorage.getItem(currentBin + VERSION_SUFFIX);
+        if (versions) {
+            try { versions = JSON.parse(versions); }
+            catch(e) {
+                versions = {};
+                console.warn('Deleting versions, they appear corrupted:', localStorage.getItem(currentBin + VERSION_SUFFIX));
+                localStorage.removeItem(currentBin + VERSION_SUFFIX);
+                versions = addVersion(versions);
+            }
+        } else {
+            versions = {};
+            versions = addVersion(versions);
+        }
+
+        return versions;
+    }
+
+    function setupBinVersioning() {
+        setInterval(() => {
+            const versions = getVersions();
+            const versionNumbers = Object.keys(versions);
+            versionNumbers.sort();
+            if (field.value.trim() !== versions[versionNumbers[versionNumbers.length-1]].trim()) {
+                addVersion(versions);
+            }
+        }, VERSION_DELAY);
+    }
+
+    function addVersion(versions) {
+        const ts = Date.now();
+        versions[ts] = field.value;
+        const versionNumbers = Object.keys(versions);
+        if (versionNumbers.length > MAX_VERSIONS) {
+            versionNumbers.sort();
+            for (let i=0; i<(versionNumbers.length - MAX_VERSIONS); ++i) {
+                delete versions[versionNumbers[i]];
+            }
+        }
+
+        localStorage.setItem(currentBin + VERSION_SUFFIX, JSON.stringify(versions));
+        document.querySelector('.version-list').innerHTML = buildVersionList();
+        return versions;
+    }
+
+    function setupTopMenus() {
+        const binList = document.querySelector('.notes-list');
+        binList.innerHTML += buildBinList();
+
+        const versionList = document.querySelector('.version-list');
+        versionList.innerHTML = buildVersionList();
 
         document.body.addEventListener('click', (e) => {
             if (e.target.className === 'new-note') {
@@ -68,26 +120,55 @@
                 } else if (e.target.className === 'bin')  {
                     switchNote(e.target.innerText);
                 }
-                toggleList();
-                
+                toggleList(binList);
+            
+            } else if (e.target.className === 'version') {
+                setToVersion(e.target.getAttribute('data-ts'));
+                toggleList(versionList);
+
+            } else if (e.target.className === 'versions') {
+                toggleList(versionList);
+
             } else if (e.target.className === 'notes-title' || e.target.parentNode.className === 'notes-title') {
-                toggleList();
+                toggleList(binList);
 
             } else {
-                list.style.display = 'none';
+                binList.style.display = 'none';
+                versionList.style.display = 'none';
             }
         });
 
-        function toggleList() {
+        function toggleList(list) {
             list.style.display = (list.style.display === 'block') ? 'none' : 'block';
         }
     }
 
     function buildBinList() {
-        const bins = Object.keys(localStorage);
+        const bins = Object.keys(localStorage).filter((bin) => { return !/__versions$/.test(bin); });
         return (bins.map((bin) => {
             return `<li class='note-link'><button class='delete' title='Delete this note'>X</button><span class='bin'>${bin}</span></li>`;
         })).join('');
+    }
+
+    function buildVersionList() {
+        const versions = getVersions();
+        return (Object.keys(versions).sort().map((ts) => {
+            return `<li class='version' data-ts='${ts}'>${(new Date(Number(ts))).toLocaleString()}</li>`;
+        })).join('');
+    }
+
+    function setToVersion(version) {
+        console.log('reverting to version:', version);
+        const versions = getVersions();
+
+        if (!versions[version] && versions[version] !== '') {
+            return alert('That was not a valid version, it may have been removed. Maybe try again?');
+        } else {
+            field.value = versions[version];
+            localStorage.setItem(currentBin, field.value);
+            delete versions[version];
+            addVersion(versions);
+        }
     }
 
     function deleteNote(bin) {
@@ -103,6 +184,7 @@
             localStorage.setItem(bin, '');
         } else {
             localStorage.removeItem(bin);
+            localStorage.removeItem(bin + VERSION_SUFFIX);
             document.querySelectorAll('.note-link').forEach((link) => {
                 if (link.querySelector('.bin').innerText === bin) {
                     link.parentNode.removeChild(link);
@@ -169,5 +251,5 @@
             content: pastLines[pastLines.length-1] + lineEnd
         }
     }
-    window.getCurrentLine = getCurrentLine;
+    
 })();
