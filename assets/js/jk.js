14e2623bee8de1a76bfa1ef38cfe408cd6f6e5e9
Jordan Kasper <jordan@jordankasper.com>
2025-07-15 22:17:56 -0400

    The start of a better (simple) analytics solution

--- assets/js/jk.js
+++ assets/js/jk.js
@@ -1,9 +1,10 @@
-(function () {
+(async function () {
 
     setupHobbesHover()
     adjustContentHeight()
     setupSearch()
     setupImageLinks()
+    await setupAnalytics()
 
     function setupHobbesHover() {
         const hobbes = $('.hobbes-hover')
@@ -32,10 +33,11 @@
             showImages: false,
             excerptLength: 30,
             showEmptyFilters: false,
-            resetStyles: false
+            resetStyles: false,
+            highlightParam: 'q'
         }
-        new PagefindUI({ element: ".search", ...options })
-        new PagefindUI({ element: "#off-canvas-nav .search", ...options })
+        new PagefindUI({ element: '.search', ...options })
+        new PagefindUI({ element: '#off-canvas-nav .search', ...options })
 
         $('.pagefind-ui__drawer').css('maxHeight', `${$('.main-content').height()-240}px`)
 
@@ -46,10 +48,38 @@
         })
     }
 
-    function setupImageLinks() {
+    async function setupImageLinks() {
         Array.from($('.blog-post img')).forEach((n) => {
             $(n).after(`<p class='view-image'><a href='${n.getAttribute('src')}' target='_blank'>[view]</a></p>`)
         })
+    }
+
+    async function setupAnalytics() {
+        var fp = await (await FingerprintJS.load()).get()
+
+        const visitData = {
+            id: fp.visitorId,
+            p: window.location.path,
+            q: window.location.search,
+            ref: document.referrer,
+            ua: navigator.userAgent,
+            ts: Date.now(),
+            tz: Intl.DateTimeFormat().resolvedOptions().timeZone,
+            bounce: 0
+        }
+
+        // TODO:
+        // Store data in localStorage, send on interval (after X amount of time has passed)
+        //  - need to track pages and page _views_ (count), but maybe with a timeout?
+        //  - track searches?
+        // Check for window unload, send data (and indicate bounce, but just on that last page)
+
+        const resp = await fetch(`/.netlify/functions/processVisit?data=${btoa(JSON.stringify(visitData))}`)
+        if (resp.status === 200) {
+            console.log(await resp.json())
+        } else {
+            console.debug(`analytic API failed: ${resp.status}: ${await resp.text()}`)
+        }
     }
 
 })()
--- plugins/copy.js
+++ plugins/copy.js
@@ -3,14 +3,19 @@ const fs = require('fs-extra');
 const logger = require('./_logger')();
 
 module.exports = function copy(opts){
-  const locations = opts.locations || []
+  const locations = opts.locations || [];
 
   return function (files, metalsmith, done){
     logger.info('Copying static files');
 
     locations.forEach(loc => {
-      logger.debug(`copying ${loc.from} to ${loc.to}`)
-      fs.copySync(loc.from, loc.to);
+      logger.debug(`copying ${loc.from} to ${loc.to}`);
+      if (/fingerprintjs/.test(loc.from)) {
+        let content = fs.readFileSync(loc.from).toString().replace('.001', '.00000001');
+        fs.writeFileSync(loc.to, content)
+      } else {
+        fs.copySync(loc.from, loc.to);
+      }
     });
 
     done();
